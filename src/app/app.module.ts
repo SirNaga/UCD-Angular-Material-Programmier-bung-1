a7a8a8633ed5e60435c37d322ecd2ff64d7d2ea7
Lukas Cislo <45951987+SirNaga@users.noreply.github.com>
2023-12-10 16:10:19 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DataComponent } from './dashboard/data/data.component';
+import { AddDataComponent } from './dashboard/add-data/add-data.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './dashboard/button/button.component';
+import { PaginationPipe } from './dashboard/data/pagination.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatSelectModule} from "@angular/material/select";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AddDataComponent,
+    DataComponent,
+    HeaderComponent,
+    ButtonComponent,
+    PaginationPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    BrowserModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dashboard/data/data.component.ts
@@ -0,0 +1,55 @@
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import { BackendService } from 'src/app/shared/backend.service';
+import { CHILDREN_PER_PAGE } from 'src/app/shared/constants';
+import { StoreService } from 'src/app/shared/store.service';
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.scss']
+})
+export class DataComponent implements OnInit {
+
+  constructor(public storeService: StoreService, private backendService: BackendService) {}
+  @Input() currentPage!: number;
+  @Output() selectPageEvent = new EventEmitter<number>();
+  public page: number = 0;
+  pageSize: number = CHILDREN_PER_PAGE;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit(): void {
+    this.backendService.getChildren(this.currentPage);
+  }
+
+  getAge(birthDate: string) {
+    var today = new Date();
+    var birthDateTimestamp = new Date(birthDate);
+    var age = today.getFullYear() - birthDateTimestamp.getFullYear();
+    var m = today.getMonth() - birthDateTimestamp.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDateTimestamp.getDate())) {
+        age--;
+    }
+    return age;
+  }
+
+  selectPage(i: any) {
+    let currentPage = i;
+    this.selectPageEvent.emit(currentPage)
+    this.backendService.getChildren(currentPage);
+  }
+
+  public returnAllPages() {
+    return Math.ceil(this.storeService.childrenTotalCount / CHILDREN_PER_PAGE)
+  }
+
+  public cancelRegistration(childId: string) {
+    this.backendService.deleteChildData(childId, this.currentPage);
+  }
+  onPageChange(event: PageEvent): void {
+    this.currentPage = event.pageIndex + 1;
+    this.selectPage(this.currentPage);
+  }
+}
+
+
